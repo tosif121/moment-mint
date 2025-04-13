1b6126dae20427090196758d3667a3329930f82e
tosif121 <tosifraza8867@gmail.com>
2025-04-13 15:25:03 +0530

    some fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </head>
       <body className={`antialiased bg-[#0d0d17] scroll-smooth ${poppins.className}`}>
